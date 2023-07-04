54c6217489a6bb6b0a75557591676bfcd2e191f1
Pedro <pedrojuniorifpb@gmail.com>
2023-07-04 10:12:51 -0300

    Adicionando pagina de Search

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { PageStyle } from "../style";
+
+import Title from "../../components/Title";
+import Modal from "../../components/Modal";
+import SearchBar from "../../components/SearchBar";
+
+export default function Search() {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleFilter = () => {
+        setModalVisible(!modalVisible);
+    }
+
+    return (
+        <PageStyle>
+            <SearchBar callbackFilter={() => setModalVisible(true)} />
+            <Title text="Pagina em Construção" />
+            <Modal
+                visible={modalVisible}
+                buttonCallback={handleFilter}
+            />
+        </PageStyle>
+    )
+}
