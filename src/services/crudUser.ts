568849bf7363b5e73783f17cc69a0b308c5be536
Pedro <pedrojuniorifpb@gmail.com>
2023-07-13 23:30:35 -0300

    Adicionando requisições para CRUD de usuários

--- src/services/crudUser.ts
+++ src/services/crudUser.ts
@@ -1,15 +1,30 @@
-import { axiosPublic as axios } from "./config";
+import axios from "./config";
 
 export interface propsUser {
-    email: string;
-    username: string;
-    password: string;
-    password_confirmation: string;
+    email?: string;
+    username?: string;
+    password?: string;
+    password_confirmation?: string;
+    date_of_birthday?: string;
+    phone?: string;
+}
+
+export const getUser = async (id: string) => {
+    const user = await axios.get(`/users/${id}`)
+    return { ...user.data };
 }
 
 export const postUser = async (objUser: propsUser) => {
     const user = await axios.post("/users/", objUser)
-    return { user };
+    return { ...user.data };
 };
 
-// export const updateUser = async({ nome, email })
+export const updateUser = async (id: string, objUser: propsUser) => {
+    const user = await axios.patch(`/users/${id}`)
+    return { ...user.data };
+}
+
+export const deleteUser = async (id: string) => {
+    const user = await axios.delete(`/users/${id}`)
+    return { ...user.data };
+}
